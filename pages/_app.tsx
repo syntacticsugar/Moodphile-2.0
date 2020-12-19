da3fd99b9998d2c831eb60ec8a2e68c65315eb53
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2020-12-19 12:07:16 -0500

    Add react-bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
