d3447704a465d9fd46e2d1e18825b9a11f1143fb
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2020-12-19 14:37:05 -0500

    Add Bootstrap SASS theming support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
