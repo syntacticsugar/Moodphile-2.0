b6e8a111c5b64f33e4295b73ce20d9d513699ebc
Romy2020 <hello@romcreative.com>
2020-12-20 19:16:56 -0500

    Adds 🐖 pigs 🐖 as a test.

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+const About: React.FC = () => {
+  return (
+    <div>
+        <h1>DUMB</h1>
+    </div>
+  )
+};
+
+export default About;
---
+++ pages/pigs.tsx
@@ -0,0 +1,14 @@
+const Pigs: React.FC = () => {
+    return (
+      <div>
+          <h1>PIGS 4 u and PIGS 4 me!!!1oneone</h1>
+          <br/><br/>
+          <img src="https://i.postimg.cc/BZdB4gBQ/Whats-App-Image-2020-12-20-at-6-40-17-PM.jpg" 
+               style={{"width": "306px", "height": "400px"}}/>
+      </div>
+    )
+  };
+  
+  export default Pigs;
+
+
